c93d271b84f895ef4c433a318653ac8c30c31229
Christian Flothmann <christian.flothmann@xabbuh.de>
2013-12-17 16:55:44 +0100

    initialise the bundle

---
+++ README.md
@@ -0,0 +1,17 @@
+XabbuhCrApiBundle
+=================
+
+XabbuhCrApiBundle integrates [PHPCR API](https://github.com/marmelab/phpcr-api),
+a library to explore PHPCR repositories, into the Symfony2 framework.
+
+Installation
+------------
+
+See the [documentation](Resources/doc/index.md) for instructions on how to
+install the XabbuhCrApiBundle.
+
+License
+-------
+
+XabbuhCrApiBundle is licensed under the MIT license. See the [LICENSE](Resources/meta/LICENSE)
+for the full license text.
---
+++ XabbuhCrApiBundle.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+ * This file is part of the XabbuhCrApiBundle package.
+ *
+ * (c) Christian Flothmann <christian.flothmann@xabbuh.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Xabbuh\CrApiBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * XabbuhCrApiBundle.
+ *
+ * @author Christian Flothmann <christian.flothmann@xabbuh.de>
+ */
+class XabbuhCrApiBundle extends Bundle
+{
+}
+ 
